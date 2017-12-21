e2e17b251660956d55a1f91deae902ef6ea5a614
vsap2 <vsap29@outlook.com>
2017-12-21 02:51:21 +0200

    pow - todo mul, mod, squared - fixed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,16 +2,19 @@ import field.Field._
 
 object Program{
   def main(args: Array[String]): Unit ={
-    val a = new GF(List(1,1,1), 3)
+    val a = new GF(List(1,1,1,0,0,1), 3)
     val b = new GF(List(1,0,1), 3)
     val c = new GF(List(1,1,0), 3)
     val d = new GF(List(0,1,1))
     val e = GF(List(0,1,2))
     println("a+b=" + (a + b))
     println("a= " + a)
+    println("a*a= " + a * a)
+    println("a.squared= " + a.squared)
+    println("a^2= " + (a ^ 2))
     println("b= " + b)
     println("c= " + c)
-    println("a mod d=" + (a mod d))
+    println("a mod b=" + (a mod b))
     println("d= " + d)
     println("e= " + e)
 
